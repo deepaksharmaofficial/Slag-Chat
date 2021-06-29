786ce36f177a45a9da6eff192585b901aa996796
deepak0111 <deepak@intellectfaces.co.in>
2021-06-29 13:50:50 +0530

    adding 2

--- notebook1.js
+++ notebook1.js
@@ -1,1 +1,9 @@
-console.log("Welcome to Slack-Chat")
+var express = require(‘express’);
+
+var app = express();
+
+var server = app.listen(3000, () => {
+ console.log(‘server is running on port’, server.address().port);
+});
+
+app.use(express.static(__dirname));
